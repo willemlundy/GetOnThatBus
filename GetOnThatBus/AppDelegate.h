eca3085fecf22484b85aab1a326a215613e896b9
Ace Wingfoot Tire <Willemlundy@williams-macbook-pro.local>
2015-10-13 13:48:39 -0700

    Initial Commit

---
+++ GetOnThatBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetOnThatBus
+//
+//  Created by William Lundy on 10/13/15.
+//  Copyright © 2015 William Lundy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GetOnThatBus/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GetOnThatBus
+//
+//  Created by William Lundy on 10/13/15.
+//  Copyright © 2015 William Lundy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
